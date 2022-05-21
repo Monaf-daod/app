b9136bd18ac15063a18de289a96e58e2f2231eea
Monaf Daod <monaf2daod@gmail.com>
2022-05-22 00:57:18 +0300

    modify contact section

--- src/components/Socialmedia/Style.js
+++ src/components/Socialmedia/Style.js
@@ -2,32 +2,36 @@ import styled from "styled-components";
 
 export const SocialMedia = styled.div`
   width: 100%;
-  height: 100vh;
-  padding-top: 5rem;
+  min-height: 100vh;
   overflow: hidden;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center; */
+  justify-content: center;
   align-items: center;
+  @media (min-width: 320px) and (max-width: 767px) {
+    min-height: 100vh;
+    height: fit-content;
+  }
 `;
 
 export const Socialmediasocial = styled.div`
-  width: 33.33%;
+  min-width: 15rem;
   height: 15rem;
+  margin: 4px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 2rem;
+  border-radius: 50%;
   cursor: pointer;
   background: ${(props) => (props.info === 1 ? "#0274b3" : "")};
   background: ${(props) => (props.info === 2 ? "#bcbcbc" : "")};
   background: ${(props) => (props.info === 3 ? "#cc2127" : "")};
   background: ${(props) => (props.info === 4 ? "#25D366" : "")};
   background: ${(props) => (props.info === 5 ? "#229ED9a1" : "")};
-
   @media (min-width: 320px) and (max-width: 767px) {
-    width: 50%;
+    min-width: 9rem;
     height: 9rem;
   }
 `;
